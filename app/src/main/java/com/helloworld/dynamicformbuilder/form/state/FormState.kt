2cf28b0bd06ca2b42dd85176c6a59d6427d2e9fc
codex-yasxh <adit7ya0patanwar@outlook.com>
2026-02-01 23:29:55 +0530

    Centralised central state

--- app/src/main/java/com/helloworld/dynamicformbuilder/form/state/FormState.kt
+++ app/src/main/java/com/helloworld/dynamicformbuilder/form/state/FormState.kt
@@ -1,5 +1,18 @@
 package com.helloworld.dynamicformbuilder.form.state
 
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+
 data class FormState(
     val fields : Map<String, FieldState>
 )
+// isFormValid = all FieldState.error == null AND required filled
+// and also derivedState is function of Formstate
+
+val isFormValid by remember(formState) {
+    derivedStateOf {
+        formSchema.fields.all{
+            
+        }
+    }
+}
