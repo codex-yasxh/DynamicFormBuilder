e22004bcc599d4fde09eb0ea239b30028e704f05
codex-yasxh <adit7ya0patanwar@outlook.com>
2026-02-01 23:46:35 +0530

    passing states and callbacks

--- app/src/main/java/com/helloworld/dynamicformbuilder/form/state/FormState.kt
+++ app/src/main/java/com/helloworld/dynamicformbuilder/form/state/FormState.kt
@@ -2,6 +2,8 @@ package com.helloworld.dynamicformbuilder.form.state
 
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import com.helloworld.dynamicformbuilder.form.models.FieldSchema
+import com.helloworld.dynamicformbuilder.form.models.RootSchema
 
 data class FormState(
     val fields : Map<String, FieldState>
@@ -11,8 +13,18 @@ data class FormState(
 
 val isFormValid by remember(formState) {
     derivedStateOf {
-        formSchema.fields.all{
-            
+        RootSchema.fields.all{
+
+
+            val fieldState = formState.fields[FieldSchema.id] ?: return@derivedStateOf false
+
+            // If field is required, it must have a value and no error
+            if(fieldSchema.required){
+                fieldState.value != null && fieldState.error == null
+            }else{
+                // Optional field is valid if it has no error
+                fieldState.error == null
+            }
         }
     }
 }
