7bb157ad203b1c13b1a60155b2e31fdedfd29f61
codex-yasxh <adit7ya0patanwar@outlook.com>
2026-02-02 08:23:39 +0530

    rendering the UI

--- app/src/main/java/com/helloworld/dynamicformbuilder/form/screens/DynamicFormScreen.kt
+++ app/src/main/java/com/helloworld/dynamicformbuilder/form/screens/DynamicFormScreen.kt
@@ -1,24 +1,33 @@
 package com.helloworld.dynamicformbuilder.form.screens
 
 import android.util.Log
+import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.isTraceInProgress
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.isTraceInProgress
 import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.helloworld.dynamicformbuilder.form.engine.FieldRegistry
 import com.helloworld.dynamicformbuilder.form.fields.text.TextRenderer
 import com.helloworld.dynamicformbuilder.form.models.FieldSchema
 import com.helloworld.dynamicformbuilder.form.models.RootSchema
+import com.helloworld.dynamicformbuilder.form.preview.sampleFixtures
 import com.helloworld.dynamicformbuilder.form.state.FieldState
 import com.helloworld.dynamicformbuilder.form.state.FormState
-import java.lang.reflect.Field
+//import java.lang.reflect.Field
 import kotlin.collections.plus
-import kotlin.plus
 import kotlin.text.get
 
-@Preview(showSystemUi = true)
+//import kotlin.plus
+//import kotlin.text.get
+
+
 @Composable
 fun DynamicFormScreen(
     rootSchema: RootSchema,
@@ -34,19 +43,24 @@ fun DynamicFormScreen(
         )
     }
     // 3.1 and 3.2 Hold Form state
-    val formState = remember {
-        FormState(
-            fields = rootSchema.fields.associate { field ->
-                field.id to FieldState() // textFieldID -> textFieldState(value = null, error = null, isTouched = false)
-            }
+    var formState by remember {
+        mutableStateOf(
+            FormState(
+                fields = rootSchema.fields.associate { field ->
+                    field.id to FieldState()
+                }
+            )
         )
     }
 
-
-
+    @Composable
+    fun StaticHeader() {
+        Log.d("RECOMPOSE", "Header recomposed")
+        Text(rootSchema.screen.title)
+    }
 
      //displaying the title
-    Text(rootSchema.screen.title)
+    StaticHeader()
 
     //looping thru all the fields
     rootSchema.fields.forEach { field ->
@@ -73,6 +87,27 @@ fun DynamicFormScreen(
 
         val fieldState = formState.fields[field.id] ?: return@forEach
 
+        fun updateFieldValue(
+            fieldId : String,
+            newValue : Any?
+        ){
+            val oldFieldState = formState.fields[fieldId] ?: return //only reading the old state that how it changed
+
+            val updatedFieldState = oldFieldState.copy(
+                value = newValue,
+                isTouched = true
+            ) //created brand new object with FieldState(newValue, isTouched = true) while having the exact things as oldFieldState
+
+
+            formState = formState.copy(
+                fields = formState.fields + (fieldId to updatedFieldState) //fieldID is new id here "to" i for you know 'mapping to new field instance'
+                // left side of + is map<fieldID, FieldState()> and right side is new entry or pair
+                // now + creates a new map and insert the pair
+                // insertion rule : if key already exists, replace the value of that particular pair else entry is added
+            ) //Give me a NEW map where everything stays the same, EXCEPT this one key.
+
+        }
+
         //4.3 and 4.5
         key(field.id) {
             FieldItem(
@@ -86,28 +121,31 @@ fun DynamicFormScreen(
         }
     }
 
-}
-    fun updateFieldValue(
-        fieldId : String,
-        newValue : Any?
-    ){
-        val oldFieldState = formState.fields[fieldId] ?: return //only reading the old state that how it changed
+    // isFormValid = all FieldState.error == null AND required filled
+// and also derivedState is function of FormState
 
-        val updatedFieldState = oldFieldState.copy(
-            value = newValue,
-            isTouched = true
-        ) //created brand new object with FieldState(newValue, isTouched = true) while having the exact things as oldFieldState
+    val isFormValid by remember(formState) {
+        derivedStateOf {
+            rootSchema.fields.all{ fieldSchema ->
 
+                val fieldState = formState.fields[fieldSchema.id] ?: return@derivedStateOf false
 
-        formState = formState.copy(
-            fields = formState.fields + (fieldId to updatedFieldState) //fieldID is new id here "to" i for you know 'mapping to new field instance'
-            // left side of + is map<fieldID, FieldState()> and right side is new entry or pair
-            // now + creates a new map and insert the pair
-            // insertion rule : if key already exists, replace the value of that particular pair else entry is added
-        ) //Give me a NEW map where everything stays the same, EXCEPT this one key.
-
+                // If field is required, it must have a value and no error
+                if(fieldSchema.required){
+                    fieldState.value != null && fieldState.error == null
+                }else{
+                    // Optional field is valid if it has no error
+                    fieldState.error == null
+                }
+            }
+        }
     }
 
+
+
+
+
+}
 //4.4 only recomposing the specific affected field
 @Composable
 fun FieldItem(
@@ -120,16 +158,22 @@ fun FieldItem(
     val renderer = registry.getRenderer(field.type) ?: return
 
     renderer.Render(
-        field = FieldSchema,
-        state = FieldState,
+        field = field,
+        state = fieldState,
         onValueChange = onValueChange
     )
 }
 
+
+
+@Preview(showBackground = true)
 @Composable
-fun StaticHeader(...) {
-    Log.d("RECOMPOSE", "Header recomposed")
+fun DynamicFormScreenPreview() {
+    DynamicFormScreen(
+        rootSchema = sampleFixtures.simpleTextForm
+    )
 }
+
 
 
 
--- app/src/main/java/com/helloworld/dynamicformbuilder/form/state/FormState.kt
+++ app/src/main/java/com/helloworld/dynamicformbuilder/form/state/FormState.kt
@@ -8,23 +8,4 @@ import com.helloworld.dynamicformbuilder.form.models.RootSchema
 data class FormState(
     val fields : Map<String, FieldState>
 )
-// isFormValid = all FieldState.error == null AND required filled
-// and also derivedState is function of Formstate
 
-val isFormValid by remember(formState) {
-    derivedStateOf {
-        RootSchema.fields.all{
-
-
-            val fieldState = formState.fields[FieldSchema.id] ?: return@derivedStateOf false
-
-            // If field is required, it must have a value and no error
-            if(fieldSchema.required){
-                fieldState.value != null && fieldState.error == null
-            }else{
-                // Optional field is valid if it has no error
-                fieldState.error == null
-            }
-        }
-    }
-}
