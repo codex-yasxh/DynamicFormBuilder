75eff174f5fcfc177884825b589532a448bf8b56
codex-yasxh <adit7ya0patanwar@outlook.com>
2026-02-04 09:03:49 +0530

    conditional visibility

--- app/src/main/java/com/helloworld/dynamicformbuilder/form/models/FieldSchema.kt
+++ app/src/main/java/com/helloworld/dynamicformbuilder/form/models/FieldSchema.kt
@@ -1,6 +1,7 @@
 package com.helloworld.dynamicformbuilder.form.models
 
 import com.helloworld.dynamicformbuilder.form.validation.ValidationRule
+import com.helloworld.dynamicformbuilder.form.visibility.VisibilityRule
 
 data class FieldSchema(
     val id : String,
--- app/src/main/java/com/helloworld/dynamicformbuilder/form/models/VisibilityRule.kt
+++
@@ -1,7 +0,0 @@
-package com.helloworld.dynamicformbuilder.form.models
-
-data class VisibilityRule(
-    val dependsOn : String,
-    val condition : String,
-    val value : Any
-)
